8a6fcbb59219b112f5693b8cb45adb507784a9d0
Jonas Francisco <contato@jonasof.com.br>
2018-08-05 21:16:17 -0300

    Change fab button to center aligned button

--- app/src/main/java/jonasof/fileshortcuts/MainActivity.java
+++ app/src/main/java/jonasof/fileshortcuts/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -31,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity.this.openFile(1);
